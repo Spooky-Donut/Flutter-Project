eeb0d87fe55df8c8f6eaacf749dfea32ce5dca0c
Andrs08 <chinchillahuelvas@gmail.com>
2026-03-10 18:17:31 -0500

    Eliminacion del archivo pages.dart que no estaba siendo usado

--- app/lib/ui/widgets/top_curve_clipper.dart
+++ app/lib/ui/widgets/top_curve_clipper.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 
+//Metodo para crear una curva decorativa en la parte superior de la pantalla
 class TopCurveClipper extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
     Path path = Path();
 
+  // Altura donde empieza la curva
     double startY = size.height * 0.6;
+
+    // Qué tan profunda es la curva
     double curveDepth = size.height * 0.45;
 
     path.lineTo(0, startY);
